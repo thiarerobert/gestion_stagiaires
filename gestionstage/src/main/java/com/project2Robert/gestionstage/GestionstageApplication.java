802a42fa84d639eb9b81ed00096503b515823d6f
thiarerobert <71185500+thiarerobert@users.noreply.github.com>
2022-09-19 00:37:53 -0400

    Template générer dans spring boot.

---
+++ gestionstage/src/main/java/com/project2Robert/gestionstage/GestionstageApplication.java
@@ -0,0 +1,13 @@
+package com.project2Robert.gestionstage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionstageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionstageApplication.class, args);
+	}
+
+}
---
+++ gestionstage/src/test/java/com/project2Robert/gestionstage/GestionstageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project2Robert.gestionstage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionstageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
